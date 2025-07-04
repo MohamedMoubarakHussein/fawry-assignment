5534b65b13086a372a209808627a2907cd264aa9
Mohamed moubarak <mohmedmoberk64@gmail.com>
2025-07-04 10:12:09 +0300

    Adding  Shipment service implementation

---
+++ service/implementation/ShippingService.java
@@ -0,0 +1,13 @@
+package service.implementation;
+
+import java.util.List;
+
+import entity.contract.ShippableProduct;
+
+public class ShippingService {
+    
+    public String shipment(List<ShippableProduct> products){
+
+        return "Done";
+    }
+}
